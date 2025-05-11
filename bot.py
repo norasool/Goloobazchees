377071b8e9cf969d0b9338eb7547f8a09df2477e
norasool <Rasoolnoroozi188@gmail.com>
2025-05-12 01:07:15 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,12 @@
 import telebot
+from telebot.types import ReplyKeyboardMarkup
+
 bot = telebot.TeleBot("7993730901:AAHXLQgjMwCKf2Ai-xbZjwuBn03paEMyzcE")
+
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, " ")
+    markup = ReplyKeyboardMarkup(resize_keyboard=True)
+    markup.add("استارت")
+    bot.send_message(message.chat.id, " ", reply_markup=markup)
+
 bot.polling()
